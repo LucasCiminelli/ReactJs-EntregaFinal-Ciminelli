fe1b4d4f2ec7d1ffca7c81f7a9fbfe2c22821580
Lucas Ciminelli <lucasaciminelli@gmail.com>
2023-08-14 12:17:00 -0300

    ultimos arreglos

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -68,7 +68,7 @@ const ItemDetail = ({ item }) => {
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <path
           fill="#fcf6f6"
-          fill-opacity="1"
+          fillOpacity="1"
           d="M0,288L1440,224L1440,320L0,320Z"
         ></path>
       </svg>
